a282321fab016d01ca8451bcd079778f72c20084
Glòria Monzó <dark.aredhel@gmail.com>
2024-03-31 13:51:43 +0200

    build

--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,25 @@ import express from "express";
 import playerRoutes from "./routes/playerRoutes";
 import gameRoutes from "./routes/gameRoutes";
 import rankingRoutes from "./routes/rankingRoutes";
-
 import dotenv from 'dotenv';
+
+// Carrega les variables d'entorn des del fitxer .env
 dotenv.config();
 
-
+// Crea una instància d'Express
 const app = express();
 
-app.get("./", (req, res)=> {
-  res.send('hola mon')
-})
+// Configura una ruta d'exemple
+app.get("/", (req, res) => {
+  res.send('Hola món!');
+});
 
 // Middleware per analitzar el cos de les sol·licituds com a JSON
 app.use(express.json());
 
-// Utilitzem els routers per a cada recurs
+// Utilitza els routers per a cada recurs
 app.use("/players", playerRoutes); // Rutes del recurs jugador
 app.use("/games", gameRoutes); // Rutes del recurs joc
 app.use("/ranking", rankingRoutes); // Rutes del recurs classificació
 
-// Inicia el servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escoltant al port ${PORT}`);
-});
 export default app;
